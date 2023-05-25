913c9da6ad1e02e5b34e3fab56874675ac727eab
Dai Nguyen <nguyenldhe176088@fpt.edu.vn>
2023-05-25 10:44:46 +0700

    Add Login.jsp, index.html and changes in Login.java

--- CodeCoach/src/java/controller/Login.java
+++ CodeCoach/src/java/controller/Login.java
@@ -55,7 +55,7 @@ public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("Login.jsp").forward(request, response);
     } 
 
     /** 
