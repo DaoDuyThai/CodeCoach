69cbadd181c3b70cbd835e46045be811d5c4101b
Đào Duy Thái <daoduythai.business@gmail.com>
2023-06-12 08:56:01 +0700

    Update EmailSender.java

--- CodeCoach/src/java/controller/EmailSender.java
+++ CodeCoach/src/java/controller/EmailSender.java
@@ -6,17 +6,36 @@ import javax.mail.internet.*;
 import java.util.Properties;
 import java.util.Random;
 
+/**
+ * This class handles sending email notifications.
+ */
 public class EmailSender {
 
+    /**
+     * Default constructor.
+     */
     public EmailSender() {
     }
-    
+
+    /**
+     * Generates a random six-digit number as a string.
+     *
+     * @return The generated random number.
+     */
     public String getRandom() {
         Random rnd = new Random();
         int number = rnd.nextInt(999999);
         return String.format("%06d", number);
-    } 
+    }
 
+    /**
+     * Sends an email with the provided one-time password (OTP) to the specified
+     * recipient.
+     *
+     * @param receiverEmail The email address of the recipient.
+     * @param otp The one-time password (OTP) to be sent.
+     * @throws MessagingException if an error occurs while sending the email.
+     */
     public void sendEmail(String receiverEmail, String otp) throws MessagingException {
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
@@ -35,25 +54,30 @@ public class EmailSender {
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverEmail));
         message.setSubject("One-Time Password (OTP) Verification");
         String mess = "Dear " + receiverEmail + ",\n"
-                        + "\n"
-                        + "Thank you for using our system. To proceed with your request, please use the following One-Time Password (OTP):\n"
-                        + "\n"
-                        + "OTP: "+otp+"\n"
-                        + "\n"
-                        + "Please do not share this OTP with anyone.\n"
-                        + "\n"
-                        + "If you did not initiate this request, please ignore this email.\n"
-                        + "\n"
-                        + "Thank you,\n"
-                        + "CodeCoach\n"
-                        + "\n"
-                        + "Note: This is an automated email. Please do not reply.";
+                + "\n"
+                + "Thank you for using our system. To proceed with your request, please use the following One-Time Password (OTP):\n"
+                + "\n"
+                + "OTP: " + otp + "\n"
+                + "\n"
+                + "Please do not share this OTP with anyone.\n"
+                + "\n"
+                + "If you did not initiate this request, please ignore this email.\n"
+                + "\n"
+                + "Thank you,\n"
+                + "CodeCoach\n"
+                + "\n"
+                + "Note: This is an automated email. Please do not reply.";
         message.setText(mess);
 
         Transport.send(message);
         System.out.println("Email sent successfully to " + receiverEmail);
     }
 
+    /**
+     * Main method for testing the email sending functionality.
+     *
+     * @param args Command-line arguments.
+     */
     public static void main(String[] args) {
         EmailSender es = new EmailSender();
         try {
