21826d8546e0c2f13cafd8ae6b2acd45e944ff40
gianghv2391998 <133033824+gianghv2391998@users.noreply.github.com>
2023-06-19 14:21:09 +0700

    Update ViewAndEditAdminProfileController.java

--- CodeCoach/src/java/controller/ViewAndEditAdminProfileController.java
+++ CodeCoach/src/java/controller/ViewAndEditAdminProfileController.java
@@ -55,7 +55,7 @@ public class ViewAndEditAdminProfileController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("viewandeditadminprofile.jsp").forward(request, response);
     } 
 
     /** 
