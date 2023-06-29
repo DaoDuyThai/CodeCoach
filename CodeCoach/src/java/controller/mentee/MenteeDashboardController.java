df99142cfd981aa41cc3f6707404c55fd32ae7fe
Đào Duy Thái <daoduythai.business@gmail.com>
2023-06-29 16:02:40 +0700

    add access denied page

--- CodeCoach/src/java/controller/mentee/MenteeDashboardController.java
+++ CodeCoach/src/java/controller/mentee/MenteeDashboardController.java
@@ -72,51 +72,56 @@ public class MenteeDashboardController extends HttpServlet {
         //get user from login session
         Users user = (Users) session.getAttribute("users");
         try {
-            //get menteeId from user id 
-            int menteeId = Integer.parseInt(menteeDao.getMenteeIdbyUserId(Integer.toString(user.getUserId())));
-            //get total accepted booking
-            int totalAcceptedBooking = bookingDao.getTotalAcceptedBookingByMenteeId(menteeId);
-            //get total accepted slots
-            int totalSlot = bookingDetailDao.getTotalBookingSlotByMenteeId(menteeId);
-            //get total pending booking
-            int totalPendingBooking = bookingDao.getTotalPendingBookingByMenteeId(menteeId);
-            //get total spendings
-            int totalSpending = bookingDao.getTotalMoneySpentByMenteeId(menteeId);
-            //get monthly spending
-            //get total booking by each month and set attributes
-            int jan = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 1);
-            request.setAttribute("jan", jan);
-            int feb = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 2);
-            request.setAttribute("feb", feb);
-            int mar = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 3);
-            request.setAttribute("mar", mar);
-            int apr = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 4);
-            request.setAttribute("apr", apr);
-            int may = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 5);
-            request.setAttribute("may", may);
-            int jun = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 6);
-            request.setAttribute("jun", jun);
-            int jul = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 7);
-            request.setAttribute("jul", jul);
-            int aug = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 8);
-            request.setAttribute("aug", aug);
-            int sep = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 9);
-            request.setAttribute("sep", sep);
-            int oct = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 10);
-            request.setAttribute("oct", oct);
-            int nov = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 11);
-            request.setAttribute("nov", nov);
-            int dec = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 12);
-            request.setAttribute("dec", dec);
+            if (user == null) {
+                response.sendRedirect("accessdeniedfull.jsp");
+            } else {
+                //get menteeId from user id 
+                int menteeId = Integer.parseInt(menteeDao.getMenteeIdbyUserId(Integer.toString(user.getUserId())));
+                //get total accepted booking
+                int totalAcceptedBooking = bookingDao.getTotalAcceptedBookingByMenteeId(menteeId);
+                //get total accepted slots
+                int totalSlot = bookingDetailDao.getTotalBookingSlotByMenteeId(menteeId);
+                //get total pending booking
+                int totalPendingBooking = bookingDao.getTotalPendingBookingByMenteeId(menteeId);
+                //get total spendings
+                int totalSpending = bookingDao.getTotalMoneySpentByMenteeId(menteeId);
+                //get monthly spending
+                //get total booking by each month and set attributes
+                int jan = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 1);
+                request.setAttribute("jan", jan);
+                int feb = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 2);
+                request.setAttribute("feb", feb);
+                int mar = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 3);
+                request.setAttribute("mar", mar);
+                int apr = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 4);
+                request.setAttribute("apr", apr);
+                int may = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 5);
+                request.setAttribute("may", may);
+                int jun = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 6);
+                request.setAttribute("jun", jun);
+                int jul = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 7);
+                request.setAttribute("jul", jul);
+                int aug = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 8);
+                request.setAttribute("aug", aug);
+                int sep = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 9);
+                request.setAttribute("sep", sep);
+                int oct = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 10);
+                request.setAttribute("oct", oct);
+                int nov = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 11);
+                request.setAttribute("nov", nov);
+                int dec = bookingDetailDao.getMonthlyMoneySpentByMenteeId(menteeId, 2023, 12);
+                request.setAttribute("dec", dec);
 
-            //set data to jsp
-            request.setAttribute("totalAcceptedBooking", totalAcceptedBooking);
-            request.setAttribute("totalSlot", totalSlot);
-            request.setAttribute("totalPendingBooking", totalPendingBooking);
-            request.setAttribute("totalSpending", totalSpending);
+                //set data to jsp
+                request.setAttribute("totalAcceptedBooking", totalAcceptedBooking);
+                request.setAttribute("totalSlot", totalSlot);
+                request.setAttribute("totalPendingBooking", totalPendingBooking);
+                request.setAttribute("totalSpending", totalSpending);
 
-            //send data to jsp
-            request.getRequestDispatcher("menteedashboard.jsp").forward(request, response);
+                //send data to jsp
+                request.getRequestDispatcher("menteedashboard.jsp").forward(request, response);
+            }
+
         } catch (Exception e) {
             System.out.println(e);
         }
